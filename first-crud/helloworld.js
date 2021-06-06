b638700c604713bc93dba9662d7bed0ff660165a
Trinh Thai <ttttrinh132@gmail.com>
2021-06-06 22:56:06 +0700

    feat(express): create Hello World with express

---
+++ first-crud/helloworld.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+    console.log('Hello World1');
+    res.send('Hello World1!');
+});
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
